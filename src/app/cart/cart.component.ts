76b3238af22554035eee13384ac768da34aa5fc4
ofir aviel <ofir29200@gmail.com>
2019-06-14 19:38:43 +0300

    Add files via upload

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+ 
 import { CartService } from '../cart.service';
-
+ 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -8,10 +10,25 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent {
   items;
-
+  checkoutForm;
+ 
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
   ) {
     this.items = this.cartService.getItems();
+ 
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
+ 
+  onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+ 
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 }
